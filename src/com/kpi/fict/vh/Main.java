7f2b5fb95e9f9e8929b4b26563f3df757ecb5821
VHerashchenko <vadim.gerashchenko@gmail.com>
2021-09-08 21:37:42 +0300

    add ImmutableObject logic + main

--- src/com/kpi/fict/vh/Main.java
+++ src/com/kpi/fict/vh/Main.java
@@ -1,8 +1,38 @@
 package com.kpi.fict.vh;
 
+import com.kpi.fict.vh.immutable.ImmutableObject;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> array = Arrays.asList(1,2,3,4,5,6,7,8);
+        String str = "Immutable String";
+        int integer = 5;
+
+        DefaultObject defaultObject = new DefaultObject(array);
+        ImmutableObject immutableObject = new ImmutableObject(str, integer, defaultObject);
+
+        System.out.println(immutableObject.getImmutableString());
+        System.out.println(immutableObject.getImmutableInteger());
+        System.out.println(immutableObject.getImmutableObjectType().getNumbers());
+
+        str = "New String";
+        integer = 6;
+        defaultObject.addNumber(9);
+
+        System.out.println(immutableObject.getImmutableString());
+        System.out.println(immutableObject.getImmutableInteger());
+        System.out.println(immutableObject.getImmutableObjectType().getNumbers());
+
+        immutableObject.setString("New String");
+        immutableObject.setInteger(6);
+        immutableObject.setObject(new DefaultObject(Arrays.asList(1,2,3)));
+
+        System.out.println(immutableObject.getImmutableString());
+        System.out.println(immutableObject.getImmutableInteger());
+        System.out.println(immutableObject.getImmutableObjectType().getNumbers());
     }
 }
--- src/com/kpi/fict/vh/immutable/ImmutableObject.java
+++ src/com/kpi/fict/vh/immutable/ImmutableObject.java
@@ -1,4 +1,42 @@
 package com.kpi.fict.vh.immutable;
 
+import com.kpi.fict.vh.DefaultObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public final class ImmutableObject {
+    private final String immutableString;
+    private final Integer immutableInteger;
+    private final DefaultObject immutableObjectType;
+
+    public ImmutableObject(String str, Integer integer, DefaultObject defaultObject){
+        immutableString = str;
+        immutableInteger = integer;
+        immutableObjectType = new DefaultObject(List.copyOf(defaultObject.getNumbers()));
+    }
+
+    public ImmutableObject setString(String str){
+        return new ImmutableObject(str, this.immutableInteger, this.immutableObjectType);
+    }
+
+    public ImmutableObject setInteger(Integer integer){
+        return new ImmutableObject(this.immutableString, integer, this.immutableObjectType);
+    }
+
+    public ImmutableObject setObject(DefaultObject immutableObjectType){
+        return new ImmutableObject(this.immutableString, this.immutableInteger, immutableObjectType);
+    }
+
+    public String getImmutableString(){
+        return immutableString;
+    }
+
+    public Integer getImmutableInteger(){
+        return immutableInteger;
+    }
+
+    public DefaultObject getImmutableObjectType() {
+        return new DefaultObject(new ArrayList<>(immutableObjectType.getNumbers()));
+    }
 }
